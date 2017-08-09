a9f07f0e44697a1aca8531de6ad2652eaa0b59a3
mchacon-ml <mario.chacon@mercadolibre.com>
2017-08-09 07:24:42 -0300

    Added test driven test

---
+++ testing/testDrivenTest_test.go
@@ -0,0 +1,22 @@
+package testing
+
+// Source: https://speakerdeck.com/mitchellh/advanced-testing-with-go
+
+import "testing"
+
+func TestAddNumber(t *testing.T) {
+	cases := map[string]struct{ A, B, Expected int }{
+		"test case 01": {1, 1, 2},
+		"test case 02": {1, -1, 0},
+		"test case 03": {1, 0, 1},
+		"test case 04": {0, 0, 0},
+	}
+
+	for k, tc := range cases {
+		actual := tc.A + tc.B
+		if actual != tc.Expected {
+			t.Errorf("%s: %d + %d = %d, expected %d",
+				k, tc.A, tc.B, actual, tc.Expected)
+		}
+	}
+}
